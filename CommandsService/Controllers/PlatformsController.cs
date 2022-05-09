edab2e3d72cb86f51ff355c8664d2ccd1e1e57f1
parhambarazesh <parham.barazesh@gmail.com>
2022-05-09 13:34:59 +0200

    Add GetPlatforms method to the controller

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -6,16 +9,28 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        // we need to inject the platform repository
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService");
+            var platformItems = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
         [HttpPost]
         public ActionResult TestInboundConnection()
         {
-            Console.WriteLine($"--> Inbound post # Command Service");
+            Console.WriteLine("--> Inbound post # Command Service");
             return Ok("Inbound test from Platforms Controller");
-            
         }
     }
 }
