f7d534188b0662d81aff2a39a283e8771d9a848c
parhambarazesh <parham.barazesh@gmail.com>
2022-05-12 01:31:20 +0200

    register event processor and handler

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,4 +1,6 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
+using CommandsService.EventProcessor;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Register AppDbContext (then we can inject ICommandRepo in the controller)
 builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
-
+// we use singelton, since we are going to inject this class into message bus class subscriber class
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddControllers();
+builder.Services.AddHostedService<MessageBusSubscriber>();
 // Add automapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
